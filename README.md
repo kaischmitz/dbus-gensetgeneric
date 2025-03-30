c0d0a16787dd1884ce7d60ad440e29cdc78a97a2
Kai Schmitz <kaischmitz@yahoo.com>
2025-03-30 11:39:11 +0200

    Initial commit - based on dbus-wallboxgeneric

---
+++ README.md
@@ -0,0 +1,55 @@
+# dbus-wallboxgeneric
+Integrate any genset into Victron Energies Venus OS
+
+## Purpose
+With the scripts in this repo it should be easily possible to install, uninstall, restart a service that sets up the DBUS structure for any genset in VenusOS and GX devices from Victron.
+The idea is to fill this structure later on via MQTT. The genset then shows up in VRM.
+Forked from dbus-goecharger by vikt0rm.
+
+
+## How it works
+### Why? ###
+I am running a Vestel EVC04 wallbox. It can be controlled via Modbus TCP, however per manufacturer's spec only one single connection to the Modbus instance is allowed.
+The entire charging control in my case is running in Nodered which has the connection to the wallbox via Modbus TCP.
+So in order to integrate the measured values from the wallbox into the VRM portal I wanted to write the values from my Nodered instance to the Venus OS or VRM directly.
+
+### Details / Process
+What is the script doing:
+- Running as a service
+- connecting to DBus of the Venus OS `com.victronenergy.evcharger.generic_{DeviceInstanceID_from_config}`
+- Paths are added to the DBus with default value 0 - including some settings like name, etc
+- After that a "loop" is started which can handle updated values by callback - nothing in there yet
+- After successful DBus connection all data points need to be updated via MQTT. The coding of the data points is listed in [CCGX-Modbus-TCP-register-list.xlsx](https://github.com/victronenergy/dbus_modbustcp/blob/master/CCGX-Modbus-TCP-register-list.xlsx) in the field list under com.victronenergy.evcharger
+
+## Install & Configuration
+### Get the code
+Just grap a copy of the main branche and copy them to a folder under `/data/` e.g. `/data/dbus-wallboxgeneric`.
+After that call the install.sh script.
+
+The following script should do everything for you:
+```
+wget https://github.com/kaischmitz/dbus-wallboxgeneric/archive/refs/heads/main.zip
+unzip main.zip "dbus-wallboxgeneric-main/*" -d /data
+mv /data/dbus-wallboxgeneric-main /data/dbus-wallboxgeneric
+chmod a+x /data/dbus-wallboxgeneric/install.sh
+/data/dbus-wallboxgeneric/install.sh
+rm main.zip
+```
+⚠️ Check configuration after that - because service is already installed an running and with wrong connection data (host) you will spam the log-file
+
+### Change config.ini
+Within the project there is a file `/data/dbus-wallboxgeneric/config.ini` - just change the values - most important is the deviceinstance under "DEFAULT". More details below:
+
+| Section  | Config vlaue | Explanation |
+| ------------- | ------------- | ------------- |
+| DEFAULT  | SignOfLifeLog  | Time in minutes how often a status is added to the log-file `current.log` with log-level INFO |
+| DEFAULT  | Deviceinstance | Unique ID identifying the wallbox in Venus OS |
+| DEFAULT  | CustomName | Custom name of the wallbox  |
+| DEFAULT  | ProductName | Product name of the wallbox  |
+
+
+## Usefull links
+- https://github.com/victronenergy/dbus_modbustcp/blob/master/CCGX-Modbus-TCP-register-list.xlsx
+- https://github.com/trixing/venus.dbus-twc3
+
+## Discussions on the web
---
+++ dbus-gensetgeneric.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+ 
+# import normal packages
+import platform 
+import logging
+import sys
+import os
+import sys
+if sys.version_info.major == 2:
+    import gobject
+else:
+    from gi.repository import GLib as gobject
+import sys
+import time
+import requests # for http GET
+import configparser # for config/ini file
+ 
+# our own packages from victron
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-systemcalc-py/ext/velib_python'))
+from vedbus import VeDbusService
+
+
+class DbusGensetGenericService:
+  def __init__(self, servicename, paths, productname='generic-genset', connection='MQTT external data update'):
+    config = self._getConfig()
+    deviceinstance = int(config['DEFAULT']['Deviceinstance'])
+    customname = config['DEFAULT']['CustomName']
+    productname = config['DEFAULT']['ProductName']
+
+    self._dbusservice = VeDbusService("{}.generic_{:02d}".format(servicename, deviceinstance),register=False)
+    self._paths = paths
+    
+    logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
+    
+    paths_wo_unit = [
+      '/StatusCode',  # 0=Standby; 1..7=Startup; 8=Running; 9=Stopping; 10=Error
+      '/ErrorCode'
+    ]
+    
+    # Create the management objects, as specified in the ccgx dbus-api document
+    self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
+    self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unkown version, and running on Python ' + platform.python_version())
+    self._dbusservice.add_path('/Mgmt/Connection', connection)
+    
+    # Create the mandatory objects
+    self._dbusservice.add_path('/DeviceInstance', deviceinstance)
+    self._dbusservice.add_path('/ProductId', 0xFFFF)
+    self._dbusservice.add_path('/ProductName', productname)
+    self._dbusservice.add_path('/CustomName', customname)    
+    self._dbusservice.add_path('/FirmwareVersion', 0, writeable=True)
+    self._dbusservice.add_path('/Serial', "00000000", writeable=True)
+    self._dbusservice.add_path('/HardwareVersion', 0, writeable=True)
+    self._dbusservice.add_path('/Connected', 1)
+    self._dbusservice.add_path('/UpdateIndex', 0)
+    self._dbusservice.add_path('/RemoteStartModeEnabled', 0)
+    
+    # add paths without units
+    for path in paths_wo_unit:
+      self._dbusservice.add_path(path, None, writeable=True)
+    
+    # add path values to dbus
+    for path, settings in self._paths.items():
+      self._dbusservice.add_path(
+        path, settings['initial'], gettextcallback=settings['textformat'], writeable=True, onchangecallback=self._handlechangedvalue)
+
+    # register the service
+    self._dbusservice.register()
+
+    # last update
+    self._lastUpdate = 0
+    
+    # add _signOfLife 'timer' to get feedback in log every 5minutes
+    gobject.timeout_add(self._getSignOfLifeInterval()*60*1000, self._signOfLife)
+ 
+  def _getConfig(self):
+    config = configparser.ConfigParser()
+    config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
+    return config
+ 
+ 
+  def _getSignOfLifeInterval(self):
+    config = self._getConfig()
+    value = config['DEFAULT']['SignOfLifeLog']
+    
+    if not value: 
+        value = 0
+    
+    return int(value)
+  
+  def _signOfLife(self):
+    logging.info("--- Start: sign of life ---")
+    logging.info("Last _update() call: %s" % (self._lastUpdate))
+    logging.info("Last '/Ac/Power': %s" % (self._dbusservice['/Ac/Power']))
+    logging.info("--- End: sign of life ---")
+    return True
+ 
+ 
+  def _handlechangedvalue(self, path, value):
+  
+    # Add actions based on changed values here
+
+    # Returning True to accept the change
+    return True
+
+def main():
+  #configure logging
+  logging.basicConfig(      format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            level=logging.INFO,
+                            handlers=[
+                                logging.FileHandler("%s/current.log" % (os.path.dirname(os.path.realpath(__file__)))),
+                                logging.StreamHandler()
+                            ])
+ 
+  try:
+      logging.info("Start")
+  
+      from dbus.mainloop.glib import DBusGMainLoop
+      # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
+      DBusGMainLoop(set_as_default=True)
+     
+      #formatting 
+      _kwh = lambda p, v: (str(round(v, 2)) + 'kWh')
+      _a = lambda p, v: (str(round(v, 1)) + 'A')
+      _w = lambda p, v: (str(round(v, 1)) + 'W')
+      _v = lambda p, v: (str(round(v, 1)) + 'V')
+      _degC = lambda p, v: (str(v) + '°C')
+      _s = lambda p, v: (str(v) + 's')
+     
+      #start our main-service
+      pvac_output = DbusGensetGenericService(
+        servicename='com.victronenergy.genset',
+        paths={
+          '/Ac/L1/Power': {'initial': 0, 'textformat': _w},
+          '/Ac/L2/Power': {'initial': 0, 'textformat': _w},
+          '/Ac/L3/Power': {'initial': 0, 'textformat': _w},
+          '/Engine/CoolantTemperature': {'initial': 0, 'textformat': _degC}
+        }
+        )
+     
+      logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
+      mainloop = gobject.MainLoop()
+      mainloop.run()            
+  except Exception as e:
+    logging.critical('Error at %s', 'main', exc_info=e)
+if __name__ == "__main__":
+  main()
